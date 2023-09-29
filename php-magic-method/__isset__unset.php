51712e76587f12a5c98f0cc8401ac702731740b6
Imam Setiawan <s.imam29@gmail.com>
2023-09-29 21:15:09 +0700

    unset

--- php-magic-method/__isset__unset.php
+++ php-magic-method/__isset__unset.php
@@ -40,17 +40,40 @@ class Response extends Message
     parent::__construct($body);
   }
 
-  public function _isset($property)
+  public function __get($property)
+  {
+    if (property_exists($this, $property)) {
+      return $this->$property;
+    }
+  }
+
+  public function __set($property, $value)
+  {
+    if (property_exists($this, $property)) {
+      $this->$property = $value;
+    }
+  }
+
+  public function __isset($property)
   {
     if (isset($this->$property)) {
       return $this->$property;
     }
 
-    echo 'Property tidak ada';
+    echo 'Property tidak ada'.PHP_EOL;
 
     return false;
+  }
+
+  public function __unset($property)
+  {
+    unset($this->$property);
   }
 }
 
 $response = new Response(200, 'This is isetting and unsetting');
 var_dump(isset($response->cookie));
+
+echo $response->code.PHP_EOL;
+unset($response->code);
+var_dump($response->code);
