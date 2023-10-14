b676d7d208b9888b7372b0dc998566afaf4fdbfc
Imam Setiawan <s.imam29@gmail.com>
2023-10-14 21:32:24 +0700

    repository

---
+++ php-design-pattern/repository.php
@@ -0,0 +1,19 @@
+<?php
+
+class UserRepository
+{
+    public function getById($id)
+    {
+
+    }
+
+    public function findById($id)
+    {
+
+    }
+
+    public function retrieve()
+    {
+        
+    }
+}
